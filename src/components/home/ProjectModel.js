cfc3e4a7f4b8f14b7c01d31d8819b888a7160a24
SADAMANI,ANJUMOL <sadamani-anjumol@nissanmotor.com>
2019-07-18 22:55:44 +0530

    added create

--- src/components/home/ProjectModel.js
+++ src/components/home/ProjectModel.js
@@ -1,8 +1,24 @@
 import React, { useState } from "react";
 import { Form, Col, Row, Button } from "react-bootstrap";
 import "./projectModel.css";
+import HttpService from "../../services/httpService";
+
 const ProjectModel = () => {
-  const [projectName,setProjectName]= useState('');
+  const [projectName,setProjectName]= useState("");
+  const[ppm_id,setPpmId]=useState("");
+  const[projectDescription,setProjectDecription]=useState("");
+  const handleProjectNameOnChange = (event) => {
+    setProjectName(event.target.value)
+  }
+  const handlePpmIdOnChange = (event) => {
+    setPpmId(event.target.value)
+  }
+  const handleDescriptionOnChange = (event) => {
+    setProjectDecription(event.target.value)
+  }
+  const handleSubmit =()=>{
+HttpService.post('project',{name:{projectName},ppm_id:{ppm_id},description:{projectDescription}}).then(response).catch(error)
+  }
   return (
     <div className="project-add-new">
       <Form>
@@ -11,7 +27,7 @@ const ProjectModel = () => {
             Project Name
           </Form.Label>
           <Col sm={8}>
-            <Form.Control type="text" value={projectName}/>
+            <Form.Control type="text" value={projectName} onChange = {(event) => {handleProjectNameOnChange(event);}} />
           </Col>
         </Form.Group>
         <Form.Group as={Row} controlId="formHorizontalEmail">
@@ -19,7 +35,7 @@ const ProjectModel = () => {
             PPM_ID
           </Form.Label>
           <Col sm={8}>
-            <Form.Control type="text" />
+            <Form.Control type="text" value={ppm_id} onChange = {(event) => {handlePpmIdOnChange(event);}} />
           </Col>
         </Form.Group>
 
@@ -28,12 +44,12 @@ const ProjectModel = () => {
             Project Description
           </Form.Label>
           <Col sm={8}>
-            <Form.Control type="text" />
+            <Form.Control type="text"  value={projectDescription} onChange = {(event) => {handleDescriptionOnChange(event);}}/>
           </Col>
         </Form.Group>
         <Form.Group as={Row}>
           <Col sm={{ span: 10, offset: 2 }}>
-            <Button type="submit">Submit</Button>
+            <Button type="submit" onClick={handleSubmit}>Submit</Button>
           </Col>
         </Form.Group>
       </Form>
