a14e3c23f75152c3fc04ab5a96098e0fce10123a
Krishnakumar Narayanan <narayanan.krishnakumar@nisssanmotor.com>
2019-07-18 18:39:18 +0530

    project add page integrated

--- src/components/home.js
+++ src/components/home.js
@@ -3,6 +3,7 @@ import { Navbar, Card } from "react-bootstrap";
 import Logo from "./../assets/logo-nissan-digital.svg";
 import "./home.css";
 import HttpService from "./../services/httpService";
+import ProjectModel from "./ProjectModel";
 
 const getHeaderComponent = () => {
   return (
@@ -33,50 +34,6 @@ const Home = props => {
       .catch(err => {
         console.log(err);
       });
-    // setTimeout(() => {
-    //   setProjects([
-    //     {
-    //       projectId: 1001,
-    //       projectName: "Nissan Digital Warehouse",
-    //       ppmId: "PPM12334"
-    //     },
-    //     {
-    //       projectId: 10021,
-    //       projectName: "Nissan Digital Warehouse",
-    //       ppmId: "POLINNC2"
-    //     },
-    //     {
-    //       projectId: 10041,
-    //       projectName: "Re# First Prio",
-    //       ppmId: "PPMRXYE"
-    //     },
-    //     {
-    //       projectId: 100221,
-    //       projectName: "Nissan Digital",
-    //       ppmId: "SL78UI"
-    //     },
-    //     {
-    //       projectId: 1001,
-    //       projectName: "Nissan Digital Warehouse",
-    //       ppmId: "PPM12334"
-    //     },
-    //     {
-    //       projectId: 10021,
-    //       projectName: "Nissan Digital Warehouse",
-    //       ppmId: "POLINNC2"
-    //     },
-    //     {
-    //       projectId: 10041,
-    //       projectName: "Re# First Prio",
-    //       ppmId: "PPMRXYE"
-    //     },
-    //     {
-    //       projectId: 100221,
-    //       projectName: "Nissan Digital",
-    //       ppmId: "SL78UI"
-    //     }
-    //   ]);
-    // }, 1000);
   }, []);
 
   const gotoProjectInfo = () => {};
@@ -84,9 +41,13 @@ const Home = props => {
     <>
       {getHeaderComponent()}
       <div className="body-content d-flex">
-        <div className="add-project">Add your project</div>
+        <div className="add-project">
+          Add your project
+          <div>
+            <ProjectModel />
+          </div>
+        </div>
         <div>
-          <div> Projects </div>
           <div className="card-wrap align-items-flex-start align-content-flex-start">
             {projects &&
               projects.map((project, index) => {
@@ -99,19 +60,15 @@ const Home = props => {
                     <Card style={{ width: "320px" }}>
                       {/* style={{ width: "18rem" }} */}
                       <Card.Body>
-                        <Card.Title>{project.name}</Card.Title>
-                        {/* <Card.Text>PPMID: {project.projectId}</Card.Text> */}
+                        <Card.Title>
+                          <span className="card-title">{project.name}</span>
+                        </Card.Title>
+                        <Card.Text>Key: {project._id}</Card.Text>
                       </Card.Body>
                     </Card>
                   </div>
                 );
               })}
-
-            {/* {this.state.viewAssetList.length === 0 && (
-          <div className="d-flex justify-content-center flex-1 no-results-label">
-            <h2>No results found</h2>
-          </div>
-        )} */}
           </div>
         </div>
       </div>
